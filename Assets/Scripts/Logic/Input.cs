c7f7c632826453dcf15b3cfb67eb2551eb57421f
Max van Diepen <max.vandiepen@student.fontys.nl>
2020-10-12 15:57:21 +0200

    Input and UI functions
    - Added script for UI functions
    - Added script for input

---
+++ Assets/Scripts/Logic/Input.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Input : MonoBehaviour
+{
+    public GameEvent buttonDown;
+
+    private void OnCall()
+    {
+        buttonDown.Raise();
+    }
+}
---
+++ Assets/Scripts/UI/UIFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFunctions : MonoBehaviour
+{
+    public void ToggleVisibility()
+    {
+        this.GetComponent<Text>().enabled =! this.GetComponent<Text>().enabled;
+    }
+}
