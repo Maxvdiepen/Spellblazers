0adbb9f9ce254e1e8009255bb73279c02e890248
Max van Diepen <max.vandiepen@student.fontys.nl>
2020-10-12 15:17:51 +0200

    Syllable creation
    - made SOs for the syllables and spells.
    - made a syllable creation for the player menu.
    - added Permanent Marker font.

---
+++ Assets/ScriptableObjects/Scripts/SO_Syllable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Syllable", menuName = "ScriptableObjects/Linguistics/Syllable")]
+public class SO_Syllable : ScriptableObject
+{
+    public string syllable;
+    public AudioClip pronunciation;
+}
---
+++ Assets/Scripts/Logic/SyllableCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SyllableCreator : MonoBehaviour
+{
+    public List<SO_Syllable> allAvailableSyllables;
+    public GameObject slotPrefab;
+
+    void Start()
+    {
+        for (int i = 0; i < allAvailableSyllables.Count; i++)
+        {
+            GameObject syllableSlot = Instantiate(slotPrefab, transform);
+            syllableSlot.GetComponentInChildren<Text>().text = allAvailableSyllables[i].syllable;
+        }
+    }
+}
