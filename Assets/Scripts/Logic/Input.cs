20bfb473b57d66efee3212d1436edcbdbd12dccc
Max van Diepen <max.vandiepen@student.fontys.nl>
2020-10-28 12:58:52 +0100

    Input + GameEvents
    - Added a function for all available input possibilities, which raise their respective GameEvents.
    - Added said GameEvents.
    - Changed the submit and clear inputs to be controlled by the right joystick, up and down respectively.

--- Assets/Scripts/Logic/Input.cs
+++ Assets/Scripts/Logic/Input.cs
@@ -6,9 +6,51 @@ using UnityEngine.InputSystem;
 public class Input : MonoBehaviour
 {
     public SO_GameEvent buttonDown;
+    public SO_GameEvent moveCursor;
+    public SO_GameEvent submitSolution;
+    public SO_GameEvent clearSolution;
+    public SO_GameEvent selectPrefix;
+    public SO_GameEvent selectRoot;
+    public SO_GameEvent selectPUSuffix;
+    public SO_GameEvent selectSuffix;
 
     private void OnCall()
     {
         buttonDown.Raise();
+    }
+
+    private void OnNavigate()
+    {
+        moveCursor.Raise();
+    }
+
+    private void OnSubmit()
+    {
+        submitSolution.Raise();
+    }
+
+    private void OnClear()
+    {
+        clearSolution.Raise();
+    }
+
+    private void OnL1()
+    {
+        selectPrefix.Raise();
+    }
+
+    private void OnL2()
+    {
+        selectRoot.Raise();
+    }
+
+    private void OnR1()
+    {
+        selectPUSuffix.Raise();
+    }
+
+    private void OnR2()
+    {
+        selectSuffix.Raise();
     }
 }
